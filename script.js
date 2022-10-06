afe906ec7f9f66761960e20699192b8a8065dc7d
nolans42 <114963614+nolans42@users.noreply.github.com>
2022-10-06 14:00:17 +1100

    Copied over project

---
+++ script.js
@@ -0,0 +1,45 @@
+let weather = {
+  paris: {
+    temp: 19.7,
+    humidity: 80,
+  },
+  tokyo: {
+    temp: 17.3,
+    humidity: 50,
+  },
+  lisbon: {
+    temp: 30.2,
+    humidity: 20,
+  },
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100,
+  },
+  oslo: {
+    temp: -5,
+    humidity: 20,
+  },
+};
+let city = prompt("Enter a city?");
+city = city.toLowerCase();
+if (weather[city] !== undefined) {
+  let temperature = weather[city].temp;
+  let humidity = weather[city].humidity;
+  let celsiusTemperature = Math.round(temperature);
+  let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
+
+  alert(
+    `It is currently ${celsiusTemperature}°C (${fahrenheitTemperature}°F) in ${city} with a humidity of ${humidity}%`
+  );
+} else {
+  alert(
+    `Sorry we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
+  );
+}
+
+var date = new Date();
+document.getElementById("date").innerHTML = date;
+
+let specialCountry = document.querySelector("h1#city");
+console.log(specialCountry);
+specialCountry.innerHTML = "Paris 🇫🇷";
